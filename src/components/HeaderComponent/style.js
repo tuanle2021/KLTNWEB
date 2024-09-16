143f645920ab45413ce76b471d38b5ddf913bfb1
tuanle2021 <hoangtuanle2021@gmail.com>
2024-09-16 08:55:16 +0700

    v1

---
+++ src/components/HeaderComponent/style.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import { Row } from "antd";
+export const WrapperHeader = styled(Row)`
+        padding: 20px 120px;
+        background-color: #ffd400;
+`
+export const WrappertextHeader = styled.span`
+        font-size: 18px;
+        color: #000;
+        font-weight: bold;
+        text-align: left;
+`
+export const WrapperAccoutHeader = styled.div`
+        display: flex;
+        align-items: center;
+`
---
+++ src/pages/NotfoundPage/NotfoundPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotfoundPage = () => {
+  return (
+    <div>
+      Not Found
+    </div>
+  )
+}
+
+export default NotfoundPage
