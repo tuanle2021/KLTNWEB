8ff38da9c2843623e1896fd921f73cd375d7cf93
CoongHuynh <huynhcong.dev@gmail.com>
2024-10-01 14:57:07 +0700

    #6 delete product api

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -135,9 +135,31 @@ const updateProductById = async (req, res) => {
   }
 };
 
+// Controller để xóa sản phẩm theo ID
+const deleteProductById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+
+    // Kiểm tra quyền hạn của người dùng (chỉ dành cho quản trị viên)
+    if (!req.user || !req.user.isAdmin) {
+      return res.status(403).json({ message: "Access denied" });
+    }
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.status(200).json({ message: "Product deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
+  }
+};
+
 module.exports = {
   addProduct,
   getAllProducts,
   getProductById,
   updateProductById,
+  deleteProductById,
 };
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -6,6 +6,7 @@ const {
   getAllProducts,
   getProductById,
   updateProductById,
+  deleteProductById,
 } = require("../controllers/productControllers");
 const { authentication } = require("../middleware/authenUser");
 
@@ -13,5 +14,5 @@ router.post("/products", addProduct);
 router.get("/products", getAllProducts);
 router.get("/products/:id", getProductById);
 router.put("/products/:id", authentication, updateProductById);
-
+router.delete("/products/:id", authentication, deleteProductById);
 module.exports = router;
